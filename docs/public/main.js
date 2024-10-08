1aa2407d6317a5749f52b4fa3a52661425e8a6b9
gehongyan <gehongyan1996@126.com>
2024-10-08 08:07:30 +0000

    Update from https://github.com/gehongyan/QQBot.Net/commit/8600892912184ed3594b9ddfbf2f074b11d3f128

---
+++ docs/public/main.js
@@ -0,0 +1,58 @@
+﻿export default {
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/gehongyan/QQBot.Net',
+            title: 'GitHub'
+        },
+        {
+            icon: 'box-seam-fill',
+            href: 'https://www.nuget.org/packages/QQBot.Net/',
+            title: 'NuGet'
+        },
+        {
+            icon: 'chat',
+            href: 'https://chat.xiaoheihe.cn/iugh82ns',
+            title: 'QQ 群'
+        }
+    ],
+    start: () =>
+    {
+        let target = document.getElementById("toc");
+
+        if (!target) return;
+
+        let config = { attributes: false, childList: true, subtree: true };
+        let observer = new MutationObserver((list) =>
+        {
+            for (const mutation of list)
+            {
+                if (mutation.type === "childList" && mutation.target == target)
+                {
+                    let scrollValue = localStorage.getItem("tocScroll");
+
+                    // Add event to store scroll pos.
+                    let tocDiv = target.getElementsByClassName("flex-fill")[0];
+
+                    tocDiv.addEventListener("scroll", (event) =>
+                    {
+                        if (event.target.scrollTop >= 0)
+                        {
+                            localStorage.setItem("tocScroll", event.target.scrollTop);
+                        }
+                    });
+
+                    if (scrollValue && scrollValue >= 0)
+                    {
+                        tocDiv.scroll(0, scrollValue);
+                    }
+
+                    observer.disconnect();
+                    break;
+                }
+            }
+        });
+
+        observer.observe(target, config);
+    }
+}
